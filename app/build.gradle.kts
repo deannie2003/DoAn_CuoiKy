01782926dfa869dc69a562b7759d3500adc4b373
deannie2003 <quyendzvjppr01234@gmail.com>
2023-10-24 14:53:44 +0000

    add register page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.doanck"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.doanck"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
