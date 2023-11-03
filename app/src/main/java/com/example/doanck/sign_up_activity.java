97e4fd86669ed0fbceb7098a40a8a56df9c7bc72
CAO QUOC TUAN <21522748@gm.uit.edu.vn>
2023-11-03 21:24:04 +0700

    update xml 3 man hinh

---
+++ app/src/main/java/com/example/doanck/sign_up_activity.java
@@ -0,0 +1,14 @@
+package com.example.doanck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_activity);
+    }
+}
