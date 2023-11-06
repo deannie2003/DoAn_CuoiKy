a806daceec3479bd7019c283e70e7e57751ce2da
CAO QUOC TUAN <21522748@gm.uit.edu.vn>
2023-11-06 23:07:07 +0700

    Update sign-in

--- app/src/main/java/com/example/doanck/API/ApiInterface.java
+++ app/src/main/java/com/example/doanck/API/ApiInterface.java
@@ -1,16 +1,43 @@
 package com.example.doanck.API;
 
-import android.telecom.Call;
 
 import com.example.doanck.Model.AssetToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
 
 public interface ApiInterface {
 
     //Link API : https://uiot.ixxc.dev/auth/realms/master/protocol/openid-connect/token
 
+
+    //khởi tạo gson
+    Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd HH:mm:ss")
+            .create();
+
+    //khởi tạo retrofit
+    ApiInterface apiInterface = new Retrofit.Builder()
+            .baseUrl("https://uiot.ixxc.dev/")//domain name
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(ApiInterface.class);
+
+
     @FormUrlEncoded
-    @POST("/auth/realms/master/protocol/openid-connect/token")
-    Call<AccessToken>
+    @POST("/auth/realms/master/protocol/openid-connect/token") //phan con lai cua domain name
+    Call<AssetToken> authenticate(
+            @Field("client_id") String clientId,
+            @Field("username") String username,
+            @Field("password") String password,
+            @Field("email") String email,
+            @Field("grant_type") String grantType
+    );
 }
--- app/src/main/java/com/example/doanck/sign_in_activity.java
+++ app/src/main/java/com/example/doanck/sign_in_activity.java
@@ -1,26 +1,85 @@
 package com.example.doanck;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.doanck.API.ApiInterface;
+import com.example.doanck.Model.AssetToken;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class sign_in_activity extends AppCompatActivity {
-    private Button btnBack2;
+
+    Button bttn_sign_in, bttn_sign_up, bttn_back_to_homePage;
+    EditText userName,Email,Password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        btnBack2 = findViewById(R.id.button2);
-        btnBack2.setOnClickListener(new View.OnClickListener() {
+        loadElement();
+
+
+        //nhấn button trở về home page
+        bttn_back_to_homePage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(sign_in_activity.this, homePageActivity.class);
+                Intent intent = new Intent(sign_in_activity.this ,homePageActivity.class);
                 startActivity(intent);
             }
         });
+        // nhấn button xử lý => vào dashboard/thông báo lỗi
+        bttn_sign_in.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+              Signin();
+            }
+        });
+
+    }
+
+
+    //Hàm để load các phần tử trong UI
+    private void loadElement(){
+        bttn_sign_in = findViewById(R.id.button_sign_in);
+        bttn_back_to_homePage = findViewById(R.id.button_homepage);
+        userName = findViewById(R.id.user_name);
+        Email = findViewById(R.id.email);
+        Password = findViewById(R.id.password);
+    }
+    //Hàm để xử lý đăng nhập
+    private void Signin(){
+        ApiInterface.apiInterface.authenticate("openremote",userName.toString(),Password.toString(),Email.toString(),"password")
+                .enqueue(new Callback<AssetToken>() {
+                    //call API bat dong bo
+                    @Override
+                    public void onResponse(Call<AssetToken> call, Response<AssetToken> response) {
+                        if(response.isSuccessful()){
+                            AssetToken assetToken = response.body();
+                            String token = assetToken.getAccessToken();
+                            Toast.makeText(sign_in_activity.this,"Đăng Nhập Thành Công!",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(sign_in_activity.this,dash_board_activity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(sign_in_activity.this, "Đăng nhập thất bại!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<AssetToken> call, Throwable t) {
+                        Toast.makeText(sign_in_activity.this, "Call API lỗi !!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
