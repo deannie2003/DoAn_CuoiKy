c4f3324a8b5a8328f8f70776150c4059ecc2e4f4
HoangQuy121 <21522531@gm.uit.edu.vn>
2023-11-06 16:11:31 +0700

    update ui

--- app/src/main/java/com/example/doanck/sign_in_activity.java
+++ app/src/main/java/com/example/doanck/sign_in_activity.java
@@ -2,13 +2,25 @@ package com.example.doanck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class sign_in_activity extends AppCompatActivity {
-
+    private Button btnBack2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        btnBack2 = findViewById(R.id.button2);
+        btnBack2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(sign_in_activity.this, homePageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
