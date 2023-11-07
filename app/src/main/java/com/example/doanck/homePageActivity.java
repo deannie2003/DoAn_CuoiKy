a9fdbf19e3c6e80a94956d78189998bc237ba2e9
HoangQuy121 <21522531@gm.uit.edu.vn>
2023-11-07 22:19:06 +0700

    update c

--- app/src/main/java/com/example/doanck/homePageActivity.java
+++ app/src/main/java/com/example/doanck/homePageActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-<<<<<<< HEAD
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,9 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
-=======
+
 import android.widget.PopupMenu;
->>>>>>> 99112b6d4aba7f5dabef23d6f8defa121381fab7
+
 
 
 public class homePageActivity extends AppCompatActivity {
@@ -57,68 +56,34 @@ public class homePageActivity extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
-
-<<<<<<< HEAD
-=======
-
-        languageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Tạo PopupMenu và gán nó với Button
-                PopupMenu popupMenu = new PopupMenu(homePageActivity.this, view);
-                popupMenu.getMenuInflater().inflate(R.menu.languages_menu, popupMenu.getMenu());
-
-                // Xử lý sự kiện khi một ngôn ngữ được chọn từ PopupMenu
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem menuItem) {
-                        switch (menuItem.getItemId()) {
-                            case R.id.english:
-                                // Xử lý khi chọn tiếng Anh
-                                break;
-                            case R.id.vietnamese:
-                                // Xử lý khi chọn tiếng Việt
-                                break;
-                        }
-                        return true;
-                    }
-                });
-
-                // Hiển thị PopupMenu
-                popupMenu.show();
-            }
-        });
-
->>>>>>> 99112b6d4aba7f5dabef23d6f8defa121381fab7
-    }
-    private void LoadElement(){
-        btn_sign_in = findViewById(R.id.button);
-        btn_sign_up = findViewById(R.id.button2);
-        img = findViewById(R.id.imageButton);
-        txt_title = findViewById(R.id.HomepageTxt);
-        txt_or = findViewById(R.id.textView2);
-        btn_google = findViewById(R.id.button3);
-        btn_reset = findViewById(R.id.button4);
-    }
-    private void ChangeLanguage(){
-        isChanged =!isChanged;
-        if(isChanged){
-            img.setImageResource(R.drawable.icon_vietnam);
-            txt_title.setText("Dự báo khí tượng");
-            btn_sign_in.setText("ĐĂNG NHẬP");
-            txt_or.setText("HOẶC");
-            btn_sign_up.setText("ĐĂNG KÝ");
-            btn_google.setText("ĐĂNG NHẬP VỚI GOOGLE");
-            btn_reset.setText("Bạn quên mật khẩu?");
-        }else{
-            img.setImageResource(R.drawable.icon_uk);
-            txt_title.setText("Air Quality Monitoring");
-            btn_sign_in.setText("SIGN IN");
-            txt_or.setText("OR");
-            btn_sign_up.setText("SIGN UP");
-            btn_google.setText("CONTINUE WITH GOOGLE");
-            btn_reset.setText("Forgot your password?");
+        private void LoadElement(){
+            btn_sign_in = findViewById(R.id.button);
+            btn_sign_up = findViewById(R.id.button2);
+            img = findViewById(R.id.imageButton);
+            txt_title = findViewById(R.id.HomepageTxt);
+            txt_or = findViewById(R.id.textView2);
+            btn_google = findViewById(R.id.button3);
+            btn_reset = findViewById(R.id.button4);
         }
-    }
+        private void ChangeLanguage(){
+            isChanged =!isChanged;
+            if(isChanged){
+                img.setImageResource(R.drawable.icon_vietnam);
+                txt_title.setText("Dự báo khí tượng");
+                btn_sign_in.setText("ĐĂNG NHẬP");
+                txt_or.setText("HOẶC");
+                btn_sign_up.setText("ĐĂNG KÝ");
+                btn_google.setText("ĐĂNG NHẬP VỚI GOOGLE");
+                btn_reset.setText("Bạn quên mật khẩu?");
+            }else{
+                img.setImageResource(R.drawable.icon_uk);
+                txt_title.setText("Air Quality Monitoring");
+                btn_sign_in.setText("SIGN IN");
+                txt_or.setText("OR");
+                btn_sign_up.setText("SIGN UP");
+                btn_google.setText("CONTINUE WITH GOOGLE");
+                btn_reset.setText("Forgot your password?");
+            }
+        }
 
 }
